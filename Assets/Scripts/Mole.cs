2325c13f869c0502db18b300953e2b261f163e2a
poojapol01 <polpooja05@gmail.com>
2023-10-07 23:38:48 +0530

    Added Scripts and Logic

---
+++ Assets/Scripts/Mole.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour {
+
+    public float maxVisibleHeight = 0.2f;
+    public float hiddenHeight = -0.3f;
+    public float speed = 4f;
+    public float disappearDuration = 0.5f;
+
+    private Vector3 targetPosition;
+    private float disappearTimer = 0f;
+    private bool attackable = false;
+    private AudioSource hitSound;
+
+    // Use this for initialization
+    void Awake() {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            hiddenHeight,
+            transform.localPosition.z
+            );
+
+        transform.localPosition = targetPosition;
+    }
+
+    void Start() {
+        hitSound = GetComponentInChildren<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        disappearTimer -= Time.deltaTime;
+        if (disappearTimer <= 0f) {
+            Hide();
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
+    }
+
+    public void Rise() {
+        attackable = true;
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            maxVisibleHeight,
+            transform.localPosition.z
+            );
+
+        disappearTimer = disappearDuration;
+    }
+
+    public bool OnHit() {
+        if (hitSound.isPlaying) {
+            hitSound.Stop();
+        }
+        hitSound.Play();
+
+        Hide();
+        if (attackable) {
+            attackable = false;
+            return true;
+        }
+        return false;
+    }
+
+    private void Hide() {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            hiddenHeight,
+            transform.localPosition.z
+            );
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Hammer hammer;
+    public int score = 0;
+
+    private GameController gameController;
+
+    // Use this for initialization
+    void Start() {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space)) {
+            RaycastHit raycastHit;
+            if (Physics.Raycast(transform.position, mainCamera.transform.forward, out raycastHit)) {
+                Debug.Log("ray hit something: " + raycastHit.transform.name);
+                bool gameStart = gameController.IsGameStart;
+                if (raycastHit.transform.GetComponent<Mole>() != null && gameStart) {
+                    Mole mole = raycastHit.transform.GetComponent<Mole>();
+                    bool isMoleAttackable = mole.OnHit();
+                    hammer.Hit(mole.transform.position);
+                    if (isMoleAttackable) {
+                        score++;
+                    }
+                }
+
+                if (raycastHit.transform.name.Equals("machine") && !gameStart) {
+                    gameController.StartGame();
+                }
+            }
+        }
+    }
+}
